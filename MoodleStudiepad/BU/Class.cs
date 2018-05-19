14c488c38d0200ddce022acd02a4699075f6dfe1
Wesley van Loon <KingTamotsu@outlook.com>
2018-05-19 20:22:44 +0200

    Added the other classes from CD and added MainMenu form.

---
+++ MoodleStudiepad/BU/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodleStudiepad.BU
+{
+    public class Class {
+        private string name;
+        private List<Student> students;
+    }
+}
