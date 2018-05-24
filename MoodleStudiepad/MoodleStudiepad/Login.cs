411583a277ffc5c7ed08342fd384e9f2c3ef5d5c
Wesley van Loon <KingTamotsu@outlook.com>
2018-05-24 13:51:59 +0200

    Added menustrip (first setup with dummy data)

--- MoodleStudiepad/MoodleStudiepad/Login.cs
+++ MoodleStudiepad/MoodleStudiepad/Login.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MoodleStudiepad
+namespace MoodleStudiepad.GUI
 {
     public partial class Login : Form
     {
--- MoodleStudiepad/MoodleStudiepad/MainMenu.Designer.cs
+++ MoodleStudiepad/MoodleStudiepad/MainMenu.Designer.cs
@@ -23,32 +23,84 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mainMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inschrijvenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uitschrijvenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // menuStrip1
             // 
-            this.button1.Location = new System.Drawing.Point(713, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Logout";
-            this.button1.UseVisualStyleBackColor = true;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainMenuToolStripMenuItem,
+            this.studieToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mainMenuToolStripMenuItem
+            // 
+            this.mainMenuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logoutToolStripMenuItem});
+            this.mainMenuToolStripMenuItem.Name = "mainMenuToolStripMenuItem";
+            this.mainMenuToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.mainMenuToolStripMenuItem.Text = "Main Menu";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // studieToolStripMenuItem
+            // 
+            this.studieToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inschrijvenToolStripMenuItem,
+            this.uitschrijvenToolStripMenuItem});
+            this.studieToolStripMenuItem.Name = "studieToolStripMenuItem";
+            this.studieToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.studieToolStripMenuItem.Text = "Studie";
+            // 
+            // inschrijvenToolStripMenuItem
+            // 
+            this.inschrijvenToolStripMenuItem.Name = "inschrijvenToolStripMenuItem";
+            this.inschrijvenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inschrijvenToolStripMenuItem.Text = "Inschrijven";
+            // 
+            // uitschrijvenToolStripMenuItem
+            // 
+            this.uitschrijvenToolStripMenuItem.Name = "uitschrijvenToolStripMenuItem";
+            this.uitschrijvenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.uitschrijvenToolStripMenuItem.Text = "Uitschrijven";
             // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainMenu";
             this.Text = "MainMenu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mainMenuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inschrijvenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uitschrijvenToolStripMenuItem;
     }
 }
