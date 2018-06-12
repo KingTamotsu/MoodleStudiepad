782c570aeaf8477546e4ebc2fb5d46febcd3eed4
Justin Muris <1736302muris@zuyd.nl>
2018-06-12 10:41:53 +0200

    PI class

---
+++ MoodleStudiepad/MoodleStudiepad/BU/PrestationIndicator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodleStudiepad.BU
+{
+    public class PrestationIndicator
+    {
+        private int studentId;
+        private int courseId;
+        private int piCode;
+        private double grade;
+        private int weight;
+        private float avgGrade;
+
+        void getPrestationIndicators()
+        {
+            // return list of prestation indicators
+        }
+
+        void getAverageGrades()
+        {
+            // return avgGrade
+        }
+    }
+}
